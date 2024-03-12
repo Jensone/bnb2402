8163c90044f9f3928c7d201856efb94afef23eba
Jensone <hello@jensone.com>
2024-03-12 15:07:56 +0100

    Création d'un formulaire de contact

---
+++ src/Form/ContactType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname', TextType::class, [
+                'label' => 'Set your fisrtname :',
+                'attr' => [
+                    'placeholder' => 'Martin Martine',
+                    'class' => 'form-control mb-3',
+                ],
+                'required' => false,
+            ])
+            ->add('lastname', FileType::class)
+            ->add('message', TextareaType::class)
+            ->add('Send', SubmitType::class)
+            ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
